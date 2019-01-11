abc84ac5be4fd10fc1b894cd9299b4a82c81fc48
rosario_lissandrello <rs.lissa1997@gmail.com>
2019-01-11 09:43:49 +0100

    resuelto el problema de relative path(jsoup)

--- src/main/java/bluebomb/urlshortener/services/HTMLDownloader.java
+++ src/main/java/bluebomb/urlshortener/services/HTMLDownloader.java
@@ -3,10 +3,13 @@ package bluebomb.urlshortener.services;
 import bluebomb.urlshortener.exceptions.DownloadHTMLInternalException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+
+
 
 @Service
 public class HTMLDownloader {
@@ -18,11 +21,15 @@ public class HTMLDownloader {
      * @return HTML that contains urlToDownload
      * @throws DownloadHTMLInternalException if something go wrong in download
      */
+    
     @Cacheable("htmlPagesCache")
     public String download(String urlToDownload) throws DownloadHTMLInternalException {
         try {
             // TODO: Resolve relative path (Jsoup)
             Document doc = Jsoup.connect(urlToDownload).get();
+            Element link = doc.select("a").first();
+            String relHref = link.attr("href"); 
+            String absHref = link.attr("abs:href"); 
             return doc.html();
         } catch (IOException e) {
             throw new DownloadHTMLInternalException(e.getMessage());

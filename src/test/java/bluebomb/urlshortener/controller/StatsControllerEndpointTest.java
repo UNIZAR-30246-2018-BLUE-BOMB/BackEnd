16fccbf554707b3be2b1e740cdd7a3484e425823
Abel Chils <abelcht@outlook.es>
2019-01-14 23:45:56 +0100

    StatsConstrollerEndpointTests terminados

--- src/test/java/bluebomb/urlshortener/controller/StatsControllerEndpointTest.java
+++ src/test/java/bluebomb/urlshortener/controller/StatsControllerEndpointTest.java
@@ -13,8 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -44,12 +46,13 @@ public class StatsControllerEndpointTest {
     public void requireStaticsAboutSequenceVisited() throws DatabaseInternalException {
         databaseApi.addStats(sequenceWithViews, "Linux", "Firefox");
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<Stats>> responseEntity = restTemplate.exchange("http://localhost:"+ port
-                + "/" + sequenceWithViews + "/stats/os/daily?maxAmountOfDataToRetrieve=10",
+        ResponseEntity<List<Stats>> responseEntity = restTemplate.exchange("http://localhost:" + port
+                        + "/" + sequenceWithViews + "/stats/os/daily?maxAmountOfDataToRetrieve=10",
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<List<Stats>>(){}
-                );
+                new ParameterizedTypeReference<List<Stats>>() {
+                }
+        );
         List<Stats> response = responseEntity.getBody();
         assertEquals(1, response.size());
         Stats stats = response.get(0);
@@ -67,24 +70,111 @@ public class StatsControllerEndpointTest {
     @Test
     public void requireStaticsAboutSequenceNoneVisited() {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<Stats>> responseEntity = restTemplate.exchange("http://localhost:"+ port
+        ResponseEntity<List<Stats>> responseEntity = restTemplate.exchange("http://localhost:" + port
                         + "/" + sequenceNoneViews + "/stats/os/daily?maxAmountOfDataToRetrieve=10",
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<List<Stats>>(){}
+                new ParameterizedTypeReference<List<Stats>>() {
+                }
         );
         List<Stats> response = responseEntity.getBody();
         assertEquals(0, response.size());
     }
 
     @Test
-    public void requiereSupportedBrowsers() {
+    public void requireStaticsAboutSequenceNoneExist() {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<StatsAgent>> responseEntity = restTemplate.exchange("http://localhost:"+ port
+        try {
+            restTemplate.exchange("http://localhost:" + port
+                            + "/" + "sequenciaquenoexiste" + "/stats/os/daily?maxAmountOfDataToRetrieve=10",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<Stats>>() {
+                    }
+            );
+            assert false;
+        } catch (HttpClientErrorException e) {
+            assert e.getStatusCode() == HttpStatus.NOT_FOUND;
+        }
+    }
+
+    @Test
+    public void requireStaticsAboutUnavailableParameter() {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            restTemplate.exchange("http://localhost:" + port
+                            + "/" + sequenceNoneViews + "/stats/parametronoexiste/daily?maxAmountOfDataToRetrieve=10",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<Stats>>() {
+                    }
+            );
+            assert false;
+        } catch (HttpClientErrorException e) {
+            assert e.getStatusCode() == HttpStatus.NOT_FOUND;
+        }
+    }
+
+    @Test
+    public void requireStaticsAboutUnavailableAmountOfData() {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            restTemplate.exchange("http://localhost:" + port
+                            + "/" + sequenceNoneViews + "/stats/os/daily?maxAmountOfDataToRetrieve=-2",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<Stats>>() {
+                    }
+            );
+            assert false;
+        } catch (HttpClientErrorException e) {
+            assert e.getStatusCode() == HttpStatus.NOT_FOUND;
+        }
+    }
+
+    @Test
+    public void requireStaticsAboutStartDateAfterEndDate() {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            restTemplate.exchange("http://localhost:" + port
+                            + "/" + sequenceNoneViews + "/stats/os/daily?maxAmountOfDataToRetrieve=10&endDate=2018-12-15&startDate=2019-01-14",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<Stats>>() {
+                    }
+            );
+            assert false;
+        } catch (HttpClientErrorException e) {
+            assert e.getStatusCode() == HttpStatus.NOT_FOUND;
+        }
+    }
+
+    @Test
+    public void requireStaticsAboutEndDateAfterActualDate() {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            restTemplate.exchange("http://localhost:" + port
+                            + "/" + sequenceNoneViews + "/stats/os/daily?maxAmountOfDataToRetrieve=10&startDate=2018-12-15&endDate=3287-01-14",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<Stats>>() {
+                    }
+            );
+            assert false;
+        } catch (HttpClientErrorException e) {
+            assert e.getStatusCode() == HttpStatus.NOT_FOUND;
+        }
+    }
+
+    @Test
+    public void requireSupportedBrowsers() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<StatsAgent>> responseEntity = restTemplate.exchange("http://localhost:" + port
                         + "/browser/support",
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<List<StatsAgent>>(){}
+                new ParameterizedTypeReference<List<StatsAgent>>() {
+                }
         );
 
         List<StatsAgent> response = responseEntity.getBody();
@@ -96,13 +186,14 @@ public class StatsControllerEndpointTest {
     }
 
     @Test
-    public void requiereSupportedOs() {
+    public void requireSupportedOs() {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<StatsAgent>> responseEntity = restTemplate.exchange("http://localhost:"+ port
+        ResponseEntity<List<StatsAgent>> responseEntity = restTemplate.exchange("http://localhost:" + port
                         + "/os/support",
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<List<StatsAgent>>(){}
+                new ParameterizedTypeReference<List<StatsAgent>>() {
+                }
         );
 
         List<StatsAgent> response = responseEntity.getBody();
@@ -111,5 +202,21 @@ public class StatsControllerEndpointTest {
         assertNotNull(response);
         assertEquals(8, response.size());
         assertTrue(response.contains(new StatsAgent("Other")));
+    }
+
+    @Test
+    public void requireSupportedAgentNoneExist() {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            restTemplate.exchange("http://localhost:" + port + "/nonexist/support",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<List<StatsAgent>>() {
+                    }
+            );
+            assert false;
+        } catch (HttpClientErrorException e) {
+            assert e.getStatusCode() == HttpStatus.NOT_FOUND;
+        }
     }
 }
